2d908093b9a462c376f40ffea5102ed11552cb80
SergeiShumilin <NoiSD@yandex.ru>
2019-01-16 19:38:21 +0300

    до полинома 3 степени

--- week2_task2.py
+++ week2_task2.py
@@ -3,40 +3,50 @@ import scipy.linalg
 import matplotlib.pyplot as plt
 import math
 
+
 def f(x):
-    return math.sin(x/5)*math.exp(x/10)+5*math.exp(-x/2)
+    return math.sin(x / 5) * math.exp(x / 10) + 5 * math.exp(-x / 2)
 
-def y1(x,w):
+
+def y1(x, w):
     """Уравнение полинома первой степени"""
-    return w[0]+w[1]*x
+    return w[0] + w[1] * x
 
-def y2(x,w):
+
+def y2(x, w):
     """Уравнение полинома второй степени"""
-    return w[0]+w[1]*x+w[2]*(x**2)
+    return w[0] + w[1] * x + w[2] * (x ** 2)
 
 
-xs = np.arange(1,16) # значения х для всех полиномов
-g = [f(x) for x in xs] # исходная функция
+def y3(x, w):
+    """Уравнение полинома третьей степени"""
+    return w[0] + w[1] * x + w[2] * (x ** 2) + w[3] * (x ** 3)
+
+
+xs = np.arange(1, 16)  # значения х для всех полиномов
+g = [f(x) for x in xs]  # исходная функция
 
 # 1 степень
-A = np.array([[1,1],[1,15]],dtype=float)
-b = np.array([f(1),f(15)])
-b = np.reshape(b,(2,1))
-w = scipy.linalg.solve(A,b)
-g1 = [y1(x,w) for x in xs] # значения для полинома 1ой степени
+A = np.array([[1, 1], [1, 15]])
+b = np.array([f(1), f(15)])
+b = np.reshape(b, (2, 1))
+w = scipy.linalg.solve(A, b)
+g1 = [y1(x, w) for x in xs]  # значения для полинома 1ой степени
 
 # 2 степень
-A = np.array([[1,1,1**2],[1,6,6**2],[1,15,15**2]])
-b = np.array([f(1),f(6),f(15)])
-b = np.reshape(b,(3,1))
-w = scipy.linalg.solve(A,b)
+A = np.array([[1, 1, 1 ** 2], [1, 6, 6 ** 2], [1, 15, 15 ** 2]])
+b = np.array([f(1), f(6), f(15)])
+b = np.reshape(b, (3, 1))
+w = scipy.linalg.solve(A, b)
+g2 = [y2(x, w) for x in xs]
+
+# 3 степень
+A = np.array([[1, 1, 1 ** 2, 1 ** 3], [1, 4, 4 ** 2, 4 ** 3], [1, 10, 10 ** 2, 10 ** 3], [1, 15, 15 ** 2, 15 ** 3]])
+b = np.array([f(1), f(4), f(10), f(15)])
+b = np.reshape(b, (4, 1))
+w = scipy.linalg.solve(A, b)
+g3 = [y3(x, w) for x in xs]
 print(w)
-g2 = [y2(x,w) for x in xs]
 
-
-plt.plot(xs,g,g1,g2)
+plt.plot(xs, g, xs, g1, xs, g2, xs, g3)
 plt.show()
-
-
-
-
